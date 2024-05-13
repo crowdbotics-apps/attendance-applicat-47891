d08f348c3c2aeaad60bfc2b95f1c079ab7ab5b4b
Crowdbotics <team@crowdbotics.com>
2024-05-13 15:38:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "attendance-applicat-47891",
+  url: "https://attendance-applicat-47891.botics.co",
+  api: "https://attendance-applicat-47891.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
